0f9c2f947fb1137c0a43b6eacfaa732dcc4ea36a
Jacob-Bridges <122827480+Jacob-Bridges@users.noreply.github.com>
2024-12-16 10:41:44 -0500

    Add files via upload

---
+++ Lab03P1.py
@@ -0,0 +1,22 @@
+﻿#
+# Jacob Bridges
+# 01/24/2023
+# Inventory Estimator
+#
+
+# Get the starting numbers of paperbacks and hardbacks.
+books = int(input('What is the current number of books? '))
+dvds = int(input('What is the current number of DVDs? '))
+games = int(input('What is the current number of games? '))
+print()
+
+# Display the inventory stock table.
+for month in range(1, 4):
+    books += 45
+    dvds += 32
+    games += 15
+    print(f'Month {month}')
+    print(f'\tBooks: {books}')
+    print(f'\t DVDs: {dvds}')
+    print(f'\tGames: {games}')
+
